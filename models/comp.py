8aafdaebf104b312d7b6805b5451b9438232a4a8
mWHMoJcf <OPhSSNRq0d>
2021-11-08 18:35:23 +0200

    first commit

---
+++ models/comp.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+try:
+    from project.proj import db
+except:
+    from __main__ import db
+
+
+
+class Departments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    department = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+class Employees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    department = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    salary = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
---
+++ proj.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///company.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models.comp import Employees, Departments
+
+def avg_salaries(departments, employees):
+    dnt_salary = {}
+    for dnt in departments:
+        dnt_salary[dnt.department] = []
+    for emp in employees:
+        for dnt in departments:
+            if emp.department == dnt.department:
+                dnt_salary[dnt.department].append(emp.salary)
+    for dnt_name in dnt_salary:
+        avg_salary = None
+        if dnt_salary[dnt_name]:
+            avg_salary = 0
+            for salary in dnt_salary[dnt_name]:
+                avg_salary += salary
+            avg_salary /= len(dnt_salary[dnt_name])
+        else:
+            avg_salary = 'No employees'
+        dnt_salary[dnt_name] = avg_salary
+    return dnt_salary
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/departments', methods=['POST', 'GET'])
+def departments_page():
+    departments = Departments.query.all()
+    employees = Employees.query.all()
+    dnt_salary = avg_salaries(departments, employees)
+    if request.method == 'POST':
+        department = request.form['department']
+
+        dnt = Departments(department=department)
+        try:
+            db.session.add(dnt)
+            db.session.commit()
+            departments = Departments.query.all()
+            dnt_salary = avg_salaries(departments, employees)
+            return render_template('departments.html', departments=departments, dnt_salary=dnt_salary)
+        except:
+            'Error'
+            return render_template('departments.html', departments=departments, dnt_salary=dnt_salary)
+    else:
+        return render_template('departments.html', departments=departments, dnt_salary=dnt_salary)
+
+@app.route('/employees', methods=['POST', 'GET'])
+def employees_page():
+    departments = Departments.query.all()
+    employees = Employees.query.all()
+    if request.method == 'POST':
+        if 'department' in request.form.keys():
+            name = request.form['name']
+            department = request.form['department']
+            salary = int(request.form['salary'])
+            birth_date = datetime.datetime.strptime(request.form['birth_date'], '%Y-%m-%d')
+            print(birth_date)
+
+            emp = Employees(name=name, department=department, birth_date=birth_date, salary=salary)
+
+            try:
+                db.session.add(emp)
+                db.session.commit()
+                departments = Departments.query.all()
+                employees = Employees.query.all()
+                return render_template('employees.html', employees=employees, departments=departments)
+            except:
+                'Error'
+                return render_template('employees.html', employees=employees, departments=departments)
+        elif 'From' in request.form.keys():
+            from_date = datetime.datetime.strptime(request.form['From'], '%Y-%m-%d').date()
+            to_date = datetime.datetime.strptime(request.form['To'], '%Y-%m-%d').date()
+            print(to_date)
+            sorted_employees = []
+            if to_date >= from_date:
+                for emp in employees:
+                    if from_date <= emp.birth_date <=to_date:
+                        sorted_employees.append(emp)
+                return render_template('employees.html', employees=sorted_employees, departments=departments)
+            else:
+                error = 'Choose correct dates'
+                return render_template('employees.html', employees=employees, departments=departments)
+    departments = Departments.query.all()
+    employees = Employees.query.all()
+    return render_template('employees.html', employees=employees, departments=departments)
+
+if __name__ == '__main__':
+    app.run(debug=True)
